0bf08ff732acf810f119856212d903e942a2a339
Alexander Sobolev <sobolev.ax@gmail.com>
2019-10-28 00:39:49 +0300

    Layout for Breadcrumbs component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,14 +4,16 @@ import { CoursesListComponent } from './courses-list/courses-list.component';
 import { CoursesListItemComponent } from './courses-list-item/courses-list-item.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 
 
 @NgModule({
-  declarations: [CoursesListComponent, CoursesListItemComponent, HeaderComponent, FooterComponent],
+  declarations: [CoursesListComponent, CoursesListItemComponent, HeaderComponent, FooterComponent, BreadcrumbsComponent],
   exports: [
     CoursesListComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BreadcrumbsComponent,
   ],
   imports: [
     CommonModule
