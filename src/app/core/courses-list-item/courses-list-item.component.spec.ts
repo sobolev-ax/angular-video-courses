398183c37ed026c2fff2448f450d7457a2bc2bcd
Alexander Sobolev <sobolev.ax@gmail.com>
2019-11-05 01:30:50 +0300

    Added Stand Alone testing

--- src/app/core/courses-list-item/courses-list-item.component.spec.ts
+++ src/app/core/courses-list-item/courses-list-item.component.spec.ts
@@ -2,10 +2,25 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoursesListItemComponent } from './courses-list-item.component';
 import { CoursesListItem } from '../../interfaces/courses-list-item';
+import { By } from '@angular/platform-browser';
 
 describe('CoursesListItemComponent', () => {
   let component: CoursesListItemComponent;
   let fixture: ComponentFixture<CoursesListItemComponent>;
+  let mockItem: CoursesListItem;
+
+  const el = {
+    debug: {
+      desc: null,
+      delete: null,
+      edit: null,
+    },
+    native: {
+      desc: null,
+      delete: null,
+      edit: null,
+    },
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,6 +35,23 @@ describe('CoursesListItemComponent', () => {
     fixture = TestBed.createComponent(CoursesListItemComponent);
     component = fixture.componentInstance;
 
+    // find the DebugElement and element
+    el.debug.desc = fixture.debugElement.query(By.css('.item-main-description'));
+    el.native.desc = el.debug.desc.nativeElement;
+    el.debug.edit = fixture.debugElement.query(By.css('#item-control-el-edit'));
+    el.native.edit = el.debug.edit.nativeElement;
+    el.debug.delete = fixture.debugElement.query(By.css('#item-control-el-delete'));
+    el.native.delete = el.debug.delete.nativeElement;
+
+    // mock the course supplied by the parent component
+    mockItem = {
+      Id: 0,
+      Title: 'mock-title',
+      CreationDate: 'mock-date',
+      Duration: 'mock-duration',
+      Description: 'mock-description',
+    };
+
     // simulate the parent setting the input property
     component.item = mockItem;
 
@@ -29,12 +61,31 @@ describe('CoursesListItemComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
 
-const mockItem: CoursesListItem = {
-  Id: 0,
-  Title: 'mock-title',
-  CreationDate: 'mock-date',
-  Duration: 'mock-duration',
-  Description: 'mock-description',
-};
+  it('should display description in uppercase', () => {
+    const expectedPipedName = mockItem.Description.toUpperCase();
+    mockItem.Description = expectedPipedName;
+
+    fixture.detectChanges();
+
+    expect(el.native.desc.textContent).toContain(expectedPipedName);
+  });
+
+  it('should raise editCourse event when clicked (triggerEventHandler)', () => {
+    let selectedId: Number;
+
+    component.editCourse.subscribe((id: Number) => selectedId = id);
+    el.debug.edit.triggerEventHandler('click', null);
+
+    expect(selectedId).toBe(mockItem.Id);
+  });
+
+  it('should raise deleteCourse event when clicked (triggerEventHandler)', () => {
+    let selectedId: Number;
+
+    component.deleteCourse.subscribe((id: Number) => selectedId = id);
+    el.debug.delete.triggerEventHandler('click', null);
+
+    expect(selectedId).toBe(mockItem.Id);
+  });
+});
