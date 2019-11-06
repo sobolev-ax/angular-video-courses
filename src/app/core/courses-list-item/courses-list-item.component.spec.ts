83d2486db4bbe0157c910025c10196156bb0c6db
Alexander Sobolev <sobolev.ax@gmail.com>
2019-11-07 00:12:22 +0300

    Added tests host

--- src/app/core/courses-list-item/courses-list-item.component.spec.ts
+++ src/app/core/courses-list-item/courses-list-item.component.spec.ts
@@ -2,90 +2,76 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoursesListItemComponent } from './courses-list-item.component';
 import { CoursesListItem } from '../../interfaces/courses-list-item';
-import { By } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 describe('CoursesListItemComponent', () => {
-  let component: CoursesListItemComponent;
-  let fixture: ComponentFixture<CoursesListItemComponent>;
-  let mockItem: CoursesListItem;
+  let testHost: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
 
-  const el = {
-    debug: {
-      desc: null,
-      delete: null,
-      edit: null,
-    },
-    native: {
-      desc: null,
-      delete: null,
-      edit: null,
-    },
-  };
+  let editBtnEl: HTMLElement;
+  let deleBtnEl: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        CoursesListItemComponent,
-      ],
+      declarations: [CoursesListItemComponent, TestHostComponent],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CoursesListItemComponent);
-    component = fixture.componentInstance;
+    fixture  = TestBed.createComponent(TestHostComponent);
+    testHost = fixture.componentInstance;
 
-    // find the DebugElement and element
-    el.debug.desc = fixture.debugElement.query(By.css('.item-main-description'));
-    el.native.desc = el.debug.desc.nativeElement;
-    el.debug.edit = fixture.debugElement.query(By.css('#item-control-el-edit'));
-    el.native.edit = el.debug.edit.nativeElement;
-    el.debug.delete = fixture.debugElement.query(By.css('#item-control-el-delete'));
-    el.native.delete = el.debug.delete.nativeElement;
-
-    // mock the course supplied by the parent component
-    mockItem = {
-      Id: 0,
-      Title: 'mock-title',
-      CreationDate: 'mock-date',
-      Duration: 'mock-duration',
-      Description: 'mock-description',
-    };
-
-    // simulate the parent setting the input property
-    component.item = mockItem;
+    editBtnEl = fixture.nativeElement.querySelector('#item-control-el-edit');
+    deleBtnEl = fixture.nativeElement.querySelector('#item-control-el-delete');
 
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display full information of course', () => {
+    const main: HTMLElement = fixture.nativeElement.querySelector('.item-main');
+
+    expect(main.textContent).toContain(mockCourse.Title);
+    expect(main.textContent).toContain(mockCourse.CreationDate);
+    expect(main.textContent).toContain(mockCourse.Duration);
+    expect(main.textContent).toContain(mockCourse.Description);
+
+    expect(Object.keys(mockCourse).length).toBe(5);
   });
 
-  it('should display description in uppercase', () => {
-    const expectedPipedName = mockItem.Description.toUpperCase();
-    mockItem.Description = expectedPipedName;
-
-    fixture.detectChanges();
-
-    expect(el.native.desc.textContent).toContain(expectedPipedName);
+  it('should raise editCourse event when clicked', () => {
+    editBtnEl.click();
+    expect(testHost.editedCourse).toBe(mockCourse.Id);
   });
 
-  it('should raise editCourse event when clicked (triggerEventHandler)', () => {
-    let selectedId: Number;
-
-    component.editCourse.subscribe((id: Number) => selectedId = id);
-    el.debug.edit.triggerEventHandler('click', null);
-
-    expect(selectedId).toBe(mockItem.Id);
-  });
-
-  it('should raise deleteCourse event when clicked (triggerEventHandler)', () => {
-    let selectedId: Number;
-
-    component.deleteCourse.subscribe((id: Number) => selectedId = id);
-    el.debug.delete.triggerEventHandler('click', null);
-
-    expect(selectedId).toBe(mockItem.Id);
+  it('should raise deleteCourse event when clicked', () => {
+    deleBtnEl.click();
+    expect(testHost.deletedCourse).toBe(mockCourse.Id);
   });
 });
+
+const mockCourse: CoursesListItem = {
+  Id: 0,
+  Title: 'Title',
+  CreationDate: 'CreationDate',
+  Duration: 'Duration',
+  Description: 'Description'
+};
+
+@Component({
+  template: `
+    <app-courses-list-item
+      [item]="course"
+      (deleteCourse)="deleteCourse($event)"
+      (editCourse)="editCourse($event)"
+    ></app-courses-list-item>`
+})
+class TestHostComponent {
+  course: CoursesListItem = mockCourse;
+
+  editedCourse: Number;
+  deletedCourse: Number;
+
+  editCourse(id: Number) { this.editedCourse = id; }
+  deleteCourse(id: Number) { this.deletedCourse = id; }
+}
