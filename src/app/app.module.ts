d1bd330347f8b5011998bfac1b054df7c81ee250
Alexander Sobolev <sobolev.ax@gmail.com>
2019-12-03 02:04:32 +0300

    Removed core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
 import { FormsModule } from '@angular/forms';
+
+
+import { CommonModule } from '@angular/common';
+import { CoursesListComponent } from './components/courses-list/courses-list.component';
+import { CoursesListItemComponent } from './components/courses-list-item/courses-list-item.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { SearchComponent } from './components/search/search.component';
+
+import { RelevanceDirective } from './directives/relevance.directive';
+import { DurationPipe } from './pipes/duration.pipe';
+import { ModalBoxComponent } from './components/modal-box/modal-box.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesListComponent,
+    CoursesListItemComponent,
+    HeaderComponent,
+    FooterComponent,
+    BreadcrumbsComponent,
+    SearchComponent,
+    RelevanceDirective,
+    DurationPipe,
+    ModalBoxComponent,
+  ],
+  exports: [
+    CoursesListComponent,
+    HeaderComponent,
+    FooterComponent,
+    BreadcrumbsComponent,
+    SearchComponent,
+    ModalBoxComponent,
   ],
   imports: [
-    BrowserModule,
-    CoreModule,
+    CommonModule,
     FormsModule,
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
